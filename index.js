3b022a72d193bb1e128fdd4690e0e415abfeb7ee
MxAyri <79870485+MxAyri@users.noreply.github.com>
2023-07-10 23:36:50 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -18,3 +18,37 @@ document.querySelectorAll('a').forEach((elem) => {
 	}
 
 })
+
+var c = document.getElementById('canvi');
+var $ = c.getContext('2d');
+
+var w = c.width = window.innerWidth;
+var h = c.height = window.innerHeight;
+
+var draw = function(a, b, t) {
+  $.lineWidth = 0.5;
+  $.fillStyle = 'hsla(266, 100%, 1%, 1)';
+  $.fillRect(0, 0, w, h);
+  for (var i = -60; i < 60; i += 1) {
+    $.strokeStyle = 'hsla(204, 94%, 74%, 1)';
+    $.beginPath();
+    $.moveTo(0, h / 2);
+    for (var j = 0; j < w; j += 10) {
+      $.lineTo(10 * Math.sin(i / 10) +
+        j + 0.008 * j * j,
+        Math.floor(h / 2 + j / 2 *
+          Math.sin(j / 50 - t / 50 - i / 118) +
+          (i * 0.9) * Math.sin(j / 25 - (i + t) / 65)));
+    };
+    $.stroke();
+  }
+}
+var t = 0;
+
+var run = function() {
+  window.requestAnimationFrame(run);
+  t += 5;
+  draw(33, 52 * Math.sin(t / 2400), t);
+};
+
+run();
