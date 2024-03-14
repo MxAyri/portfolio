cc5ec06bb06941dbe8d8e04dfc9f78118588bc79
MxAyri <79870485+MxAyri@users.noreply.github.com>
2024-03-14 14:44:57 +0300

    addMovie1

--- index.js
+++ index.js
@@ -19,21 +19,21 @@ document.querySelectorAll('a').forEach((elem) => {
 
 })
 
-var c = document.getElementById('canvi');
-var $ = c.getContext('2d');
+let c = document.getElementById('canvi');
+let $ = c.getContext('2d');
 
-var w = c.width = window.innerWidth;
-var h = c.height = window.innerHeight;
+let w = c.width = window.innerWidth;
+let h = c.height = window.innerHeight;
 
-var draw = function(a, b, t) {
+let draw = function(a, b, t) {
   $.lineWidth = 0.5;
   $.fillStyle = 'hsla(266, 100%, 1%, 1)';
   $.fillRect(0, 0, w, h);
-  for (var i = -60; i < 60; i += 1) {
+  for (let i = -60; i < 60; i += 1) {
     $.strokeStyle = 'hsla(204, 94%, 74%, 1)';
     $.beginPath();
     $.moveTo(0, h / 2);
-    for (var j = 0; j < w; j += 10) {
+    for (let j = 0; j < w; j += 10) {
       $.lineTo(10 * Math.sin(i / 10) +
         j + 0.008 * j * j,
         Math.floor(h / 2 + j / 2 *
@@ -43,9 +43,9 @@ var draw = function(a, b, t) {
     $.stroke();
   }
 }
-var t = 0;
+let t = 0;
 
-var run = function() {
+let run = function() {
   window.requestAnimationFrame(run);
   t += 5;
   draw(33, 52 * Math.sin(t / 2400), t);
@@ -55,6 +55,11 @@ run();
 
 //Nav
 let flag=0;
+
+let nav = document.querySelectorAll(".openNav");
+for(let i =0; i < nav.length; i++){
+  nav[i].addEventListener('click',openNav);
+}
 
 function openNav() {
     if (flag === 0){
